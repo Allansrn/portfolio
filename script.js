8eaa6ceb22a8690d889de5831e02650ff1ad8ba8
Allan <150249889+Allansrn@users.noreply.github.com>
2025-03-18 15:39:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ document.querySelectorAll('.veille-card').forEach(card => {
         }
     });
 });
+
+const textArray = ["web et logiciel junior"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        document.querySelector(".typing").textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        document.querySelector(".typing").textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(type, newTextDelay + 250);
+});
