6f45b67818c9702aa54b257e38c77cf0c5e1b00f
Allan <150249889+Allansrn@users.noreply.github.com>
2025-04-05 18:13:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,60 @@ document.querySelectorAll('.veille-card').forEach(card => {
         }
     });
 });
+
+const textArray = ["web et logiciel junior"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        document.querySelector(".typing").textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        document.querySelector(".typing").textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(type, newTextDelay + 250);
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Script chargé");
+    const toggleSwitch = document.getElementById("dark-mode-toggle");
+    const body = document.body;
+
+    if (localStorage.getItem("dark-mode") === "enabled") {
+        body.classList.add("dark-mode");
+        toggleSwitch.checked = true;
+    }
+
+    toggleSwitch.addEventListener("change", () => {
+        if (toggleSwitch.checked) {
+            console.log("Mode sombre activé");
+            body.classList.add("dark-mode");
+            localStorage.setItem("dark-mode", "enabled");
+        } else {
+            console.log("Mode sombre désactivé");
+            body.classList.remove("dark-mode");
+            localStorage.setItem("dark-mode", "disabled");
+        }
+    });
+});
